01fde03310bf605b304d575fdb5ec5cef063cb56
Marshall Brock <marshallbrock@gmail.com>
2021-06-06 18:47:40 -0500

    records wav and mp4 but not together

--- Assets/NatSuite/Examples/Giffy/Giffy.cs
+++ Assets/NatSuite/Examples/Giffy/Giffy.cs
@@ -12,6 +12,8 @@ namespace NatSuite.Examples {
     using Recorders.Inputs;
     using UnityEngine.UI;
     using Recorders.Clocks;
+    using NatSuite.Devices;
+
 
     public class Giffy : MonoBehaviour {
         [Header("Marshalls Stuff")]
@@ -32,6 +34,12 @@ namespace NatSuite.Examples {
         private IClock clock;
         private bool recording;
         private Color32[] pixelBuffer;
+
+
+        //audio
+        private IAudioDevice audioDevice;
+        private WAVRecorder audioRecorder;
+
 
         /*
         private void Awake()
@@ -81,21 +89,50 @@ namespace NatSuite.Examples {
         public void StartRecording()
         {
             //recorderTexture = findCamera.GetCurrentCam();
-            
+
+            var frameRate = 30;
+            var sampleRate = audioDevice.sampleRate;//outputSampleRate
+            var channelCount = audioDevice.channelCount;
+            //recorder = new MP4Recorder(videoWidth, videoHeight, frameRate, sampleRate, channelCount, audioBitRate: 96_000);
+
             // Start recording
             clock = new RealtimeClock();
-            mp4Recorder = new MP4Recorder(recorderTexture.width, recorderTexture.height, 30);
+            mp4Recorder = new MP4Recorder(recorderTexture.width, recorderTexture.height, frameRate, sampleRate, channelCount, audioBitRate: 96_000);
             pixelBuffer = recorderTexture.GetPixels32();
             recording = true;
+
+            //audio
+            // Create a recorder
+            audioRecorder = new WAVRecorder(audioDevice.sampleRate, audioDevice.channelCount);
+            // Start the microphone
+            audioDevice.StartRunning((sampleBuffer, timestamp) => {
+                // And pass sample buffers directly to the recorder
+                audioRecorder.CommitSamples(sampleBuffer, timestamp);
+            });
         }
         public async void StopRecording()
         {
+            // Stop streaming audio samples to the recorder
+            audioDevice.StopRunning();
+            // Then stop recording
+            var audioPath = await audioRecorder.FinishWriting();
+
+            Debug.Log($"Saved audio to: {audioPath}");
+
+
             // Stop recording
             recording = false;
-            var path = await mp4Recorder.FinishWriting();
+            //--how to preview...should you upload then preview
+            //if you are connected to the wifi an
+
+            var videoPath = await mp4Recorder.FinishWriting();
+
             // Playback recording
-            Debug.Log($"Saved recording to: {path}");
-            Handheld.PlayFullScreenMovie($"file://{path}");
+            Debug.Log($"Saved recording to: {videoPath}");
+            Handheld.PlayFullScreenMovie($"file://{videoPath}");
+
+
+            //-if you are not connected to wifi 
         }
 
         //IEnumerator Start()
@@ -115,6 +152,14 @@ namespace NatSuite.Examples {
             cameraImage.texture = webCamTexture;
             //aspectFitter.aspectRatio = (float)webCamTexture.width / webCamTexture.height;
             //return new WaitUntil(() => webCamTexture.width != 16 && webCamTexture.height != 16);
+
+
+            //-microphone
+            var criterion = MediaDeviceQuery.Criteria.AudioDevice;
+            var query = new MediaDeviceQuery(criterion);
+            audioDevice = query.currentDevice as AudioDevice;
+            Debug.Log($"{audioDevice}");
+
         }
 
         void Update()
