0ffe3b725ea5410b42bbcef49269aae29c61ebe2
Marshall Brock <marshallbrock@gmail.com>
2021-06-06 18:12:17 -0500

    trying to get rid of UI elements

--- Assets/NatSuite/Examples/Giffy/Giffy.cs
+++ Assets/NatSuite/Examples/Giffy/Giffy.cs
@@ -12,6 +12,8 @@ namespace NatSuite.Examples {
     using Recorders.Inputs;
     using UnityEngine.UI;
     using Recorders.Clocks;
+    using NatSuite.Devices;
+
 
     public class Giffy : MonoBehaviour {
         [Header("Marshalls Stuff")]
@@ -20,112 +22,89 @@ namespace NatSuite.Examples {
         private WebCamTexture recorderTexture;
 
 
-        [Header("GIF Settings")]
-        //public int imageWidth = 640;
-        //public int imageHeight = 480;
-        public float frameDuration = 0.1f; // seconds
+
+        [Header(@"Recording")]
+        public int videoWidth = 1080;
+        public int videoHeight = 1920;
+        public bool recordMicrophone;
 
         //private GIFRecorder recorder;
         private CameraInput cameraInput;
         private WebCamTexture webCamTexture;
         private MP4Recorder mp4Recorder;
-        private IClock clock;
+        //private IClock clock;
         private bool recording;
         private Color32[] pixelBuffer;
 
-        /*
-        private void Awake()
+
+        //audio
+        private AudioInput audioInput;
+        private AudioSource microphoneSource;
+
+
+
+
+
+        private IEnumerator Start()
         {
             cameraImage = findCamera.background;
+            recorderTexture = findCamera.GetCurrentCam();
+            //recorderTexture = new WebCamTexture(1280, 720, 30);
+            //recorderTexture.Play();
+            ////yield return new WaitUntil(() => recorderTexture.width != 16 && recorderTexture.height != 16); // Workaround for weird bug on macOS
+            //cameraImage.texture = webCamTexture;
 
+
+            //new stuff
+            // Start microphone
+            microphoneSource = gameObject.AddComponent<AudioSource>();
+                microphoneSource.mute =
+                microphoneSource.loop = true;
+                microphoneSource.bypassEffects =
+                microphoneSource.bypassListenerEffects = false;
+                microphoneSource.clip = Microphone.Start(null, true, 1, AudioSettings.outputSampleRate);
+                yield return new WaitUntil(() => Microphone.GetPosition(null) > 0);
+                microphoneSource.Play();
+            
         }
-        
-        private void Update()
-        {
-            if (cameraImage != null)
-            {
-                cameraImage = findCamera.background;
-            }
-            //findCamera.background.texture;
-
-
-        }*/
-        /*
-        public void StartRecording () {
-            // Start recording
-            recorder = new GIFRecorder(cameraImage.texture.width, cameraImage.texture.height, frameDuration);
-            cameraInput = new CameraInput(recorder, Camera.main);
-            // Get a real GIF look by skipping frames
-            //cameraInput.frameSkip = 4;
-        }
-        */
-
-
-
-        //--
-        /*
-        public async void StopRecording () {
-            // Stop the recording
-            cameraInput.Dispose();
-            var path = await recorder.FinishWriting();
-            // Log path
-            Debug.Log($"Saved animated GIF image to: {path}");
-            Application.OpenURL($"file://{path}");
-        }
-        */
-
-
-        //-mp4
-
 
         public void StartRecording()
         {
-            //recorderTexture = findCamera.GetCurrentCam();
-            
             // Start recording
-            clock = new RealtimeClock();
-            mp4Recorder = new MP4Recorder(recorderTexture.width, recorderTexture.height, 30);
-            pixelBuffer = recorderTexture.GetPixels32();
-            recording = true;
+            var frameRate = 30;
+            var sampleRate = AudioSettings.outputSampleRate;
+            var channelCount = (int)AudioSettings.speakerMode;
+            var clock = new RealtimeClock();
+            mp4Recorder = new MP4Recorder(videoWidth, videoHeight, frameRate, sampleRate, channelCount, audioBitRate: 96_000);
+            // Create recording inputs
+            cameraInput = new CameraInput(mp4Recorder, clock, Camera.main);
+            audioInput = new AudioInput(mp4Recorder, clock, microphoneSource, true);
+            // Unmute microphone
+            microphoneSource.mute = audioInput == null;
         }
+
         public async void StopRecording()
         {
+            // Mute microphone
+            microphoneSource.mute = true;
             // Stop recording
-            recording = false;
+            audioInput?.Dispose();
+            cameraInput.Dispose();
             var path = await mp4Recorder.FinishWriting();
             // Playback recording
             Debug.Log($"Saved recording to: {path}");
             Handheld.PlayFullScreenMovie($"file://{path}");
+
         }
 
-        //IEnumerator Start()
-        void Start()
+        private void OnDestroy()
         {
-            cameraImage = findCamera.background;
-            recorderTexture = findCamera.GetCurrentCam();
-            // Request camera permission
-            //yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-            //if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
-            //    yield break;
-            //// Start the WebCamTexture
-            recorderTexture = new WebCamTexture(1280, 720, 30);
-            recorderTexture.Play();
-            // Display webcam
-            //yield return new WaitUntil(() => recorderTexture.width != 16 && recorderTexture.height != 16); // Workaround for weird bug on macOS
-            cameraImage.texture = webCamTexture;
-            //aspectFitter.aspectRatio = (float)webCamTexture.width / webCamTexture.height;
-            //return new WaitUntil(() => webCamTexture.width != 16 && webCamTexture.height != 16);
+            // Stop microphone
+            microphoneSource.Stop();
+            Microphone.End(null);
         }
 
-        void Update()
-        {
-            // Record frames from the webcam
-            if (recording && recorderTexture.didUpdateThisFrame)
-            {
-                recorderTexture.GetPixels32(pixelBuffer);
-                mp4Recorder.CommitFrame(pixelBuffer, clock.timestamp);
-            }
-        }
+
 
     }
 }
